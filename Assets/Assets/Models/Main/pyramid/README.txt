9cd4f71e48c2d3f5b6320db49eb03cb1dd3d9f40
JosephNguyenNazareth <iohkgn9861@gmail.com>
2017-11-01 08:38:28 +0700

    PlayGround

---
+++ Assets/Assets/Models/Main/pyramid/README.txt
@@ -0,0 +1,32 @@
+                   .:                     :,                                          
+,:::::::: ::`      :::                   :::                                          
+,:::::::: ::`      :::                   :::                                          
+.,,:::,,, ::`.:,   ... .. .:,     .:. ..`... ..`   ..   .:,    .. ::  .::,     .:,`   
+   ,::    :::::::  ::, :::::::  `:::::::.,:: :::  ::: .::::::  ::::: ::::::  .::::::  
+   ,::    :::::::: ::, :::::::: ::::::::.,:: :::  ::: :::,:::, ::::: ::::::, :::::::: 
+   ,::    :::  ::: ::, :::  :::`::.  :::.,::  ::,`::`:::   ::: :::  `::,`   :::   ::: 
+   ,::    ::.  ::: ::, ::`  :::.::    ::.,::  :::::: ::::::::: ::`   :::::: ::::::::: 
+   ,::    ::.  ::: ::, ::`  :::.::    ::.,::  .::::: ::::::::: ::`    ::::::::::::::: 
+   ,::    ::.  ::: ::, ::`  ::: ::: `:::.,::   ::::  :::`  ,,, ::`  .::  :::.::.  ,,, 
+   ,::    ::.  ::: ::, ::`  ::: ::::::::.,::   ::::   :::::::` ::`   ::::::: :::::::. 
+   ,::    ::.  ::: ::, ::`  :::  :::::::`,::    ::.    :::::`  ::`   ::::::   :::::.  
+                                ::,  ,::                               ``             
+                                ::::::::                                              
+                                 ::::::                                               
+                                  `,,`
+
+
+http://www.thingiverse.com/thing:1852864
+Pyramid of Kukulkan at Chichen Itza by adeo1 is licensed under the Creative Commons - Attribution license.
+http://creativecommons.org/licenses/by/3.0/
+
+# Summary
+
+This is the El Castillo pyramid at Chichen Itza. It is a wonder of the world. Also called the pyramid of Kukulkan
+
+# Print Settings
+
+Printer Brand: MakerBot
+Printer: MakerBot Replicator (5th Generation)
+Resolution: .1mm
+Infill: 15%
---
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/CombatMusicControl.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class CombatMusicControl : MonoBehaviour {
+
+
+	public AudioMixerSnapshot outOfShark;
+	public AudioMixerSnapshot inShark;
+
+	public AudioMixerSnapshot outOfWolf;
+	public AudioMixerSnapshot inWolf;
+
+	public AudioMixerSnapshot outOfSpider;
+	public AudioMixerSnapshot inSpider;
+
+	public AudioMixerSnapshot outOfFinish;
+	public AudioMixerSnapshot inFinish;
+
+	public AudioMixerSnapshot outOfTreasure;
+	public AudioMixerSnapshot inTreasure;
+
+	public float bpm = 128;
+
+
+	private float m_TransitionIn;
+	private float m_TransitionOut;
+	private float m_QuarterNote;
+
+	// Use this for initialization
+	void Start () 
+	{
+		m_QuarterNote = 60 / bpm;
+		m_TransitionIn = m_QuarterNote;
+		m_TransitionOut = m_QuarterNote * 32;
+
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("FightZone"))
+		{
+			inShark.TransitionTo(m_TransitionIn);
+		} else if (other.CompareTag("WolfZone"))
+		{
+			inWolf.TransitionTo(m_TransitionIn);
+		} else if (other.CompareTag("SpiderZone"))
+		{
+			inSpider.TransitionTo(m_TransitionIn);
+		} else if (other.CompareTag("FinishZone"))
+		{
+			inFinish.TransitionTo(m_TransitionIn);
+		} else if (other.CompareTag("ChestZone"))
+		{
+			inTreasure.TransitionTo(m_TransitionIn);
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if (other.CompareTag("FightZone"))
+		{
+			outOfShark.TransitionTo(m_TransitionOut);
+		} else if (other.CompareTag("WolfZone"))
+		{
+			outOfWolf.TransitionTo(m_TransitionOut);
+		} else if (other.CompareTag("SpiderZone"))
+		{
+			outOfSpider.TransitionTo(m_TransitionOut);
+		} else if (other.CompareTag("FinishZone"))
+		{
+			outOfFinish.TransitionTo(m_TransitionOut);
+		} else if (other.CompareTag("ChestZone"))
+		{
+			outOfTreasure.TransitionTo(m_TransitionOut);
+		}
+	}
+}
